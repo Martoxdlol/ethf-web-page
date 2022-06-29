cfa0d16a6057571a4c73e02f4ec4a5a28fe6581d
Martoxdlol <martoxdlol@gmail.com>
2022-06-29 20:58:00 -0300

    Data types

---
+++ backend/src/api/calificacione/controllers/calificacione.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  calificacione controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::calificacione.calificacione');
---
+++ backend/src/api/calificacione/services/calificacione.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * calificacione service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::calificacione.calificacione');
