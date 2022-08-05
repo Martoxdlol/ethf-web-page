268a4027795601f760757933c913eedadea597c5
Martoxdlol <martoxdlol@gmail.com>
2022-08-05 14:03:54 -0300

    added asset prefix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const getStrapiURL = require('./lib/getStrapiUrl');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: getStrapiURL(''),
   images: {
     loader: "default",
     domains: ["localhost", "10.0.31.28", "cms.henryford.edu.ar"],
